34691f27384e4d0cddcd26a643613c16e2b09949
Yuliya Naletskaya <naletskaya@gmail.com>
2025-08-10 20:06:55 +0300

    fix: fix test

--- src/const/routes.ts
+++
@@ -1,7 +0,0 @@
-import MainPage from '../pages/MainPage';
-import type { RoutePages } from '../types/routePages';
-
-export const routes: RoutePages[] = [
-  { path: '/', element: MainPage },
-  // { path: '/about', element: AboutPage },
-];
